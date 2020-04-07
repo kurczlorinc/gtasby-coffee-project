12b0ff804d55c8b0fa9e5b4cf5d697a10dd9b484
kurlo <kurcz.l3@gmail.com>
2020-04-07 11:42:56 +0300

    coffeeitemsadded

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 import BackgroundSection from '../components/Globals/BackgroundSection'
 import Info from '../components/Home/info'
-
+import Menu from '../components/Home/Menu'
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -16,21 +16,40 @@ const IndexPage = ({data}) => (
       title="regular joe's"
       styleClass="default-background" />
     <Info />
+    <Menu items={data.menu}/>
   </Layout>
 )
 
 export const query = graphql`
 {
-  img:file(relativePath:{eq:"default-background.jpeg"}){
-    childImageSharp{
-      fluid{
+  img: file(relativePath: { eq: "default-background.jpeg" }) {
+    childImageSharp {
+      fluid {
         ...GatsbyImageSharpFluid_tracedSVG
       }
     }
   }
-  
+  menu: allContentfulCoffeeItem {
+    edges {
+      node {
+        id
+        title
+        description {
+          description
+        }
+        price
+        category
+        image {
+          fixed(width: 50, height: 50) {
+            ...GatsbyContentfulFixed_tracedSVG
+          }
+        }
+      }
+    }
+  }
 }
 `
+
 
 
 export default IndexPage
